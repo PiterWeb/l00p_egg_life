fc5c59d646fcac8fd54b94e715c1b108055cec1c
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2025-08-02 13:44:12 +0200

    Fullscreen implemented and hen put on scene

--- hello_world/bin/main.ml
+++ hello_world/bin/main.ml
@@ -1,6 +1,10 @@
+let window_width = 800
+let window_height = 450
+
 let setup () =
-  Raylib.init_window 1024 720 "lOOp RedCube";
-  Raylib.set_target_fps 60
+  Raylib.init_window window_width window_height "lOOp RedCube";
+  Raylib.set_target_fps 60;
+  Raylib.toggle_fullscreen ()
 
 type state = {
   player_position: Raylib.Vector3.t;
@@ -12,6 +16,15 @@ type state = {
 let player_min_speed = 1.0
 let player_base_speed = 50.0
 let player_acceleration = -5.0
+
+let full_screen _ =
+  let open Raylib in
+  if is_key_pressed Key.F11 then
+    let display = get_current_monitor () in
+    (match is_window_fullscreen () with
+    | true -> set_window_size window_width window_height
+    | false -> set_window_size (get_monitor_width display) (get_monitor_height display));
+    toggle_fullscreen ()
 
 let controls state =
   let open Raylib in
@@ -30,19 +43,19 @@ let controls state =
     let player_speed_z = z_speed +. player_acceleration *. delta_time in
     Vector3.set_x state.player_speed player_speed_x;
     Vector3.set_z state.player_speed player_speed_z;
-      if is_key_down Key.D then
-        Vector3.set_x state.player_position (x +. player_speed_x *. delta_time)
-      else if is_key_down Key.A then
-        Vector3.set_x state.player_position (x -. player_speed_x *. delta_time)
-      else if is_key_down Key.W then
-        Vector3.set_z state.player_position (z -. player_speed_z *. delta_time)
-      else if is_key_down Key.S then
-        Vector3.set_z state.player_position (z +. player_speed_z *. delta_time)
-      else (
-        Vector3.set_x state.player_speed (x_speed -. player_acceleration *. delta_time);
-        Vector3.set_z state.player_speed (z_speed -. player_acceleration *. delta_time);
-      );
-      state
+    if is_key_down Key.D then
+      Vector3.set_x state.player_position (x +. player_speed_x *. delta_time)
+    else if is_key_down Key.A then
+      Vector3.set_x state.player_position (x -. player_speed_x *. delta_time)
+    else if is_key_down Key.W then
+      Vector3.set_z state.player_position (z -. player_speed_z *. delta_time)
+    else if is_key_down Key.S then
+      Vector3.set_z state.player_position (z +. player_speed_z *. delta_time)
+    else (
+      Vector3.set_x state.player_speed (x_speed -. player_acceleration *. delta_time);
+      Vector3.set_z state.player_speed (z_speed -. player_acceleration *. delta_time);
+    );
+    state
 
 let drawing state =
   let open Raylib in
@@ -55,6 +68,8 @@ let rec loop state =
   if Raylib.window_should_close () then Raylib.close_window ()
   else
     let open Raylib in
+      full_screen ();
+      update_camera (addr state.camera) CameraMode.First_person;
       begin_drawing ();
       clear_background Color.raywhite;
       begin_mode_3d state.camera;
@@ -66,13 +81,13 @@ let rec loop state =
 
 let () =
   let camera = Raylib.Camera3D.create
-      (Raylib.Vector3.create 10.0 10.0 10.0)
-      (Raylib.Vector3.create 0.0 0.0 0.0)
-      (Raylib.Vector3.create 0.0 1.0 0.0)
-      45.0
-      Raylib.CameraProjection.Perspective in
-    let player_position = Raylib.Vector3.create 0.0 0.0 0.0 in
-    let player_speed = Raylib.Vector3.create player_base_speed 0.0 player_base_speed in
-      setup ();
-    let player_model = Raylib.load_model "./assets/Chicken/Chicken_01.obj" in
-      loop {camera; player_position; player_model; player_speed}
+    (Raylib.Vector3.create 10.0 10.0 10.0)
+    (Raylib.Vector3.create 0.0 0.0 0.0)
+    (Raylib.Vector3.create 0.0 1.0 0.0)
+    27.5
+    Raylib.CameraProjection.Orthographic in
+  let player_position = Raylib.Vector3.create 0.0 0.0 0.0 in
+  let player_speed = Raylib.Vector3.create player_base_speed 0.0 player_base_speed in
+    setup ();
+  let player_model = Raylib.load_model "./assets/hen.glb" in
+    loop {camera; player_position; player_model; player_speed}
