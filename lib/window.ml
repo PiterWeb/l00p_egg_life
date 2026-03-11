79d3aadcbd66005baad167e81029e7f52b03c4a1
PiterWeb <71133634+PiterWeb@users.noreply.github.com>
2026-03-11 13:23:56 +0100

    Resolved window blinking bug in linux

--- lib/window.ml
+++ lib/window.ml
@@ -1,9 +1,10 @@
 open Raylib
 
-let width = 800
-let height = 450
+let width = get_screen_width()
+let height = get_screen_height()
 
 let setup () =
   init_window width height "lOOp Egg life";
   set_target_fps 70;
-  toggle_fullscreen () |> hide_cursor
+  toggle_fullscreen ();
+  hide_cursor()
